2ff8e7c006a1f318b23cc4236a488363273ce6ca
spidesu <spides2009@yandex.ru>
2018-05-01 14:39:59 +0300

    Grouped Marks/Bad Ver.

--- journal/my_marks.php
+++ journal/my_marks.php
@@ -1,36 +1,46 @@
+<head>
+ <style type="text/css">
+   a.disabled {
+    cursor: default; 
+    color: #000;
+   }
+   </style>
+</head>
 <table class="table">
-<?
-	$sql="SELECT m.mark, m.date, m.description, ut.name name_teacher, l.name name_lesson FROM marks m 
-	JOIN users_teachers ut ON ut.id=m.id_teacher 
-	JOIN lessons l ON l.id=m.id_lesson  
-	WHERE m.id_student={$_SESSION['id_student']}";
-	$res=$link->query($sql);
-	$res1=$link->query($sql);$row1=$res1->fetch(PDO::FETCH_ASSOC);
-	if(!$row1['mark']){
-		echo "У вас еще нет оценок";
-	}
-	else{
-		?>
 		<tr>
-			<th>Оценка</th>
-			<th>Тема</th>
-			<th>Учитель</th>
-			<th>Урок</th>
-			<th>Дата</th>
+			<th>Предмет</th>
+			<th>Оценки</th>
 		</tr>
+<?
+
+	$sql2 = "SELECT DISTINCT l.id,l.name FROM lessons l JOIN marks m ON l.id=m.id_lesson WHERE m.id_student={$_SESSION['id_student']}";
+	$lessons=$link->query($sql2);
+
+	while($lesson_row=$lessons->fetch(PDO::FETCH_ASSOC)){
+	$sql1="SELECT m.mark, m.date, m.description, l.name name_lesson, t.name name_teacher FROM marks m 
+	JOIN lessons l ON l.id=m.id_lesson
+	JOIN users_teachers t ON m.id_teacher=t.id  
+	WHERE m.id_student={$_SESSION['id_student']} AND m.id_lesson={$lesson_row['id']}";
+	$res=$link->query($sql1);
+	$res1=$link->query($sql1);$row1=$res1->fetch(PDO::FETCH_ASSOC);
+
+		?>
+	
 		<?
-		while($row=$res->fetch(PDO::FETCH_ASSOC)){
-			echo <<<txt
+	
+			echo "
 				<tr>
-					<td>{$row['mark']}</td>
-					<td>{$row['description']}</td>
-					<td>{$row['name_teacher']}</td>
-					<td>{$row['name_lesson']}</td>
-					<td>{$row['date']}</td>
-				</tr>
-txt;
-		}
+					<td>{$lesson_row['name']}</td><td>";
+						while($row=$res->fetch(PDO::FETCH_ASSOC)){
+						echo "
+					<a href='#' class='disabled' title='Тема: {$row['description']}, Преподаватель: {$row['name_teacher']}'>{$row['mark']}</a>";}
+					echo "
+				</td></tr>";
+
+		
 	}
+
+	
 	
 ?>
 </table>
